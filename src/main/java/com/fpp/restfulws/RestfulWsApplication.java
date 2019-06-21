83d928f5069ea5ce66174e264d13accce7b64322
FULVIO POLIZEL PARMEJJANI <fulviopp@gmail.com>
2019-06-21 17:28:23 -0300

    First

---
+++ src/main/java/com/fpp/restfulws/RestfulWsApplication.java
@@ -0,0 +1,13 @@
+package com.fpp.restfulws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWsApplication.class, args);
+	}
+
+}
